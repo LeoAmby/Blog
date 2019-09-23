ea4734807b1d5ae99d326cefcb12d79ffa9a3ec9
Leo <leoamby@gmail.com>
2019-09-23 14:56:56 +0300

    create userModelTest

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db, login_manager
 from flask_login import UserMixin
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Quote:
@@ -17,7 +18,18 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(200))
     password = db.Column(db.String(100))
     name = db.Column(db.String(100))
+    pass_secure = db.Column(db.String(255))
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+                
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+    
 
-
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
+    
     def __repr__(self):
-        return f''
+        return f'User {self.username}'
