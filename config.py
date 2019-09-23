397a1e5ad93a7b7ace6e4f727c8b4a7b0fa46044
Leo <leoamby@gmail.com>
2019-09-23 11:58:30 +0300

    configure SQLAlchemy  in the config.py file

--- config.py
+++ config.py
@@ -2,8 +2,11 @@ import os
 
 
 class Config:
-    QUOTE_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'        API_KEY = os.environ.get('API_KEY')
+    QUOTE_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'        
+    API_KEY = os.environ.get('API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:psql@localhost/bloggy'
+
 
 class ProdConfig(Config):
     pass
