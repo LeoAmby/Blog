fc8ee3442bc9ffbead018a2e4e019cd838975608
Leo <leoamby@gmail.com>
2019-09-23 10:34:48 +0300

    update the __init__.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from config import config_options
+from app import app
 
 
-app = Flask(__name__)
+bootstrap = Bootstrap()
 
 from app import views
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config_options[config_name])
+
+
+    bootstrap.init_app(app)
+
+
+    return app
