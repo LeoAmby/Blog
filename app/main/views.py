b64c342f67263e0904d9866b0f6e77834f50f41f
Leo <leoamby@gmail.com>
2019-09-23 15:09:11 +0300

    create an authentification directory

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,10 @@
 from flask import render_template, request, redirect, url_for
 from . import main
+from . import auth
 # from .forms import ReviewForm
 # from ..models import Revi
+
+
+@auth.route('/login')
+def login():
+    return render_template('auth/login.html')
