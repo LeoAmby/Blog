5e8e882664362317f36b8cc125ad38acb2fbaa92
Leo <leoamby@gmail.com>
2019-09-23 16:51:49 +0300

    instantiate class login

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,13 +1,23 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, flash, request
 from . import auth
+from flask_login import login_user
 from ..models import User
-from .forms import subscriptionForm
+from .forms import subscriptionForm, LoginForm
 from .. import db
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+            flash('Invalid username of Password')
+    title = 'New User? Subscribe Here'    
+    return render_template('auth/login.html', LoginForm = LoginForm, title=title)
+
 
 
 @auth.route('/register', methods = ['GET', 'POST'])
