f4e944e423bdcf15710dcf21c4bba826366f803d
Leo <leoamby@gmail.com>
2019-09-23 11:46:20 +0300

    update manage.py

--- manage.py
+++ manage.py
@@ -1,2 +1,18 @@
 from app import create_app
+import unittest
 from flask_script import Manage, Server
+
+
+app = create_app('development')
+
+manager = manager(app)
+manager.add_command('server', Server)
+
+
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+    
+if __name__ == '__main__':
+    manager.run()
