b8697373d19065d55257aebc1f5d727718d7c8da
Leo <leoamby@gmail.com>
2019-09-24 17:47:24 +0300

    create the requests and connect it in the index.html

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,11 @@
 from flask import render_template, request, redirect, url_for
 from . import main
 from flask_login import login_required
+from ..requests import requests
 
 
 @main.route('/', methods = ['GET', 'POST'])
 @login_required
 def index():
-    # posts = Post.query.all()
-    return render_template('index.html')
+    quotes = requests()
+    return render_template('index.html', quotes = quotes)
--- app/requests.py
+++ app/requests.py
@@ -1,5 +1,6 @@
 import urllib.request, json
 from .models import Tale
+import requests, json
 
 base_url = None
 
@@ -7,3 +8,10 @@ base_url = None
 def configure_request(app):
     global base_url
     base_url = app.config['BASE_URL']
+
+def  requests():
+    request = requests.get('http://quotes.stormconsultancy.co.uk/random.json')
+    quotes = request.json()
+
+
+    return quotes
